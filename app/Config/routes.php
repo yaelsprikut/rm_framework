cd019c11d2d32ec770e4b02c4994563ee848503d
Yael L Sprikut <luck_be_lady@icloud.com>
2015-03-19 07:22:02 -0400

    CakePHP enabled on Desktop

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -32,7 +32,11 @@
  * ...and connect the rest of 'Pages' controller's URLs.
  */
 	//Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-        
+
+        Router::connect(
+        '/admin',
+        array('controller' => 'pages', 'action' => 'index', 'admin' => true)
+        );
 /**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
